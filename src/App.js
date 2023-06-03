234e68f55dd548395e246b2886dda1d72f7cdcc1
Aman Ali Saifi <amansaifi8430@gmail.com>
2023-06-04 00:37:49 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+        category : 'general'
+    }
 }
 
-export default App;
+   handleCategory = (category) =>{
+    this.setState({category})
+  } 
+  render() {
+    return (
+    <div>
+<Router>
+<Navbar handleCategory={(category)=>this.handleCategory(category)}/>
+<Switch>
+<Route  path="/"><News key= {this.state.category} pageSize={6} country='in' category={this.state.category}/></Route> 
+</Switch>
+</Router>
+    </div>
+    )
+  }
+}
+
+
